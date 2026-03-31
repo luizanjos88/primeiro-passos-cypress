63ae816100276682f8907b1a5af9d0aa1033b685
luizanjos88 <luizanjos88@hotmail.com>
2026-03-31 11:06:26 -0400

    User Update Walk add

--- cypress/e2e/loginspec.cy.js
+++ cypress/e2e/loginspec.cy.js
@@ -7,7 +7,17 @@ const selectorsList = {
     loginButton: '[type="submit"]',
     sectionTitleTopBar: '.oxd-topbar-header',
     dashboardGrid:'.orangehrm-dashboard-grid',
-    wrongCredentialAlert: '[role="alert"]'
+    wrongCredentialAlert: '[role="alert"]',
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: '[name="firstName"]',
+    middleNameField: '[name="middleName"]',
+    lastNameField:'[name="lastName"]',
+    genericField: '.oxd-input--active',
+    dateField: '.oxd-date-input',
+    closeCalendaryButton: '.--close',
+    submitButton: '[type="submit"]',
+
+
 }
 
 
@@ -16,7 +26,7 @@ beforeEach(() => {
     cy.visit('/auth/login');
   })
 
-  it('Login Sucess', () => {
+  it.only('Login Sucess', () => {
   
     cy.get(selectorsList.usernameField).type(userData.userSucess.username);
     cy.get(selectorsList.passwordField).type(userData.userSucess.password);
@@ -27,7 +37,29 @@ beforeEach(() => {
     //cy.get(selectorsList.sectionTitleTopBar).contains ('Dashboard');
 
     cy.get(selectorsList.dashboardGrid);
+
+    // acessando alteração de Usuário
+
+    cy.get(selectorsList.myInfoButton).click();
+    cy.get(selectorsList.sectionTitleTopBar).contains('PIM'),
+    cy.location('pathname').should('equal', '/web/index.php/pim/viewPersonalDetails/empNumber/7')
+    cy.get(selectorsList.firstNameField).clear().type('Luiz');
+    cy.get(selectorsList.middleNameField).clear().type('Carlos');
+    cy.get(selectorsList.lastNameField).clear().type('Anjos');
+    cy.get(selectorsList.genericField).eq(3).clear().type('QA-290305');
+    cy.get(selectorsList.genericField).eq(4).clear().type('205105');
+    cy.get(selectorsList.genericField).eq(5).clear().type('0768099909');
+    cy.get(selectorsList.dateField).eq(1).clear().type('1991-23-04');
+    cy.get(selectorsList.closeCalendaryButton).click();
+    cy.get(selectorsList.submitButton).eq(0).click();
+    cy.get('body').should('contain','Successfully Updated');
+   
+
+
   })
+
+
+
 
   it ('Login - Fail',() => {
     
@@ -37,6 +69,7 @@ beforeEach(() => {
     //texto não é recomentado porque porque pode mudar/só estou usando para testar
     // validação de alerta
     cy.get(selectorsList.wrongCredentialAlert).contains ('Invalid credentials');
+  
     
     
   })
